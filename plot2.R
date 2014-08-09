ccf4d744279fca81575839951a5d65dbaf7fb173
Jim Carson <jim.carson@gmail.com>
2014-08-09 16:57:58 -0700

    Removed cut and paste histogram.

--- plot2.R
+++ plot2.R
@@ -9,9 +9,8 @@ date_range <- c(as.Date("2007-02-01"), as.Date("2007-02-02"))
 x$dt = strptime(paste(x$Date,x$Time), "%d/%m/%Y %H:%M:%S")
 t <- x[as.Date(strptime(x$Date, "%d/%m/%Y")) %in% date_range,]
 
-# plot a histogram to a png output file.
+# plot Global active power. 
 png("plot2.png", width=480, height=480, bg="white")
-hist(t$Global_active_power, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
 plot(t, t$Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)")
 dev.off()
     
