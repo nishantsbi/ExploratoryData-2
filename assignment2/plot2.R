22b448e3e247a6cbbbd923b47365a98d941410c1
Jim Carson <jim.carson@gmail.com>
2014-08-17 17:36:26 -0700

    Assignment 2

--- assignment2/plot2.R
+++ assignment2/plot2.R
@@ -3,10 +3,6 @@ library("plyr")
 NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
 
-## Convert into data.table
-NEI.DT = data.table(NEI)
-SCC.DT = data.table(SCC)
-
 # Have total emissions from PM2.5 decreased in Baltimore from 1999 to 2008? 
 # Using the base plotting system, make a plot showing the total PM2.5 emission from Baltimore for each of the years 1999, 2002, 2005, and 2008.
 
